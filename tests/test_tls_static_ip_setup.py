31978b283bc77b9f37a70697ebbedeb203766f72
moticless <moticless@github.com>
2022-03-10 10:15:58 +0200

    Adding tls-static-ip

---
+++ tests/test_tls_static_ip_setup.py
@@ -0,0 +1,27 @@
+from .utils import *
+import subprocess
+import shutil
+import time
+
+class TestTlsStaticIpSetup:
+    SRC_DIR='docker-compose-setups/tls-static-ip/'
+    DST_DIR = f'./tmp/{SRC_DIR}'
+    SETUP_NAME = "tls-static-ip"
+
+    def setup_class(cls):
+        shutil.copytree(cls.SRC_DIR, cls.DST_DIR)
+        subprocess.check_call(["docker-compose", "-p", cls.SETUP_NAME, "up", "-d"], cwd=cls.DST_DIR)
+        time.sleep(10)
+
+    def teardown_class(cls):
+        subprocess.check_call(["docker-compose", "down"], cwd=cls.DST_DIR)
+        time.sleep(3)
+        shutil.rmtree(cls.DST_DIR)
+
+    def test_master_unavail(self):
+        failover_and_recover_init (['--times', '2', '--wait', '30', "-p", self.SETUP_NAME])
+        failover_and_recover()
+
+    def test_master_and_sentinel_unavail(self):
+        failover_and_recover_init (['--times', '2', '--wait', '30', '--sentinel_restart', "-p", self.SETUP_NAME])
+        failover_and_recover()
